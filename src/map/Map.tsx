dc962a7bd0f15e6153f4e09a3e775fc0521a645a
Natalie Frecka <natalie.frecka@uplift.delivery.com>
2024-05-22 16:09:28 -0500

    rendering simple, hardcoded route

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -2,6 +2,17 @@ import React, {FC} from 'react';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import {mapStyle} from './map-style.ts';
 import {styled} from 'tamagui';
+import {MapViewRoute} from 'react-native-maps-routes';
+
+const someFedExLocation = {
+  latitude: 41.574954467659246,
+  longitude: -93.62492891722157,
+};
+
+const uplift = {
+  latitude: 41.58394249647359,
+  longitude: -93.63353541907415,
+};
 
 const StyledMap = styled(MapView, {
   width: '100%',
@@ -12,12 +23,16 @@ export const Map: FC = () => (
   <StyledMap
     provider={PROVIDER_GOOGLE}
     initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
+      ...uplift,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     }}
-    customMapStyle={mapStyle}
-    googleMapId="dfacb6a7c2b2d486" // doesn't work
-  />
+    customMapStyle={mapStyle}>
+    <MapViewRoute
+      origin={someFedExLocation}
+      destination={uplift}
+      apiKey="<API KEY>"
+      strokeColor="red"
+    />
+  </StyledMap>
 );
