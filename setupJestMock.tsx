0d14e533d8e2e33ea22f3ef331403b701fe00943
Natalie Frecka <natalie.frecka@uplift.delivery.com>
2024-05-22 16:22:02 -0500

    fixing unit test forward ref complaint

--- setupJestMock.tsx
+++ setupJestMock.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-
 jest.mock('react-native-maps', () => {
+  const React = require('react');
   const {View} = require('tamagui');
-  const MockMapView = (props: any) => <View>{props.children}</View>;
+  const MockMapView = React.forwardRef((props: any, _: any) => (
+    <View {...props}>{props.children}</View>
+  ));
 
   return {
     __esModule: true,
     default: MockMapView,
+  };
+});
+
+jest.mock('react-native-maps-routes', () => {
+  const {View} = require('tamagui');
+  const MockView = (props: any) => <View>{props.children}</View>;
+
+  return {
+    __esModule: true,
+    MapViewRoute: MockView,
   };
 });
